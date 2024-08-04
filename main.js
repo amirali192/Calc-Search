100ce5c2b2acf6a9c18817d870c0636d7dda58b5
amirali192 <126576706+amirali192@users.noreply.github.com>
2024-08-04 14:21:07 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// CALCULATOR
+// Input
+let num1 = 12, num2 = 6;
+// Output :
+console.log("Calculator Project")
+// Sum
+console.log("Sum =", num1 + num2)
+// Subtraction
+console.log("Subtraction =", num1 - num2)
+// Multiplication
+console.log("Multiplication =", num1 * num2)
+// Division
+console.log("Division =", num1 / num2)
+// Remainder
+console.log("Remainder =", num1 % num2)
+// End of calulator
+console.log("=======================")
+
+
+
+// Search (EXIST OR DOES NOT EXIST)
+console.log("Search (EXIST OR DOES NOT EXIST) Project")
+let string = "cheeseBorgor", target = "cheese"
+if (string.includes(target) == true) {
+    console.log("found!")
+}
+else {
+    console.log("NO!")
+}
+
